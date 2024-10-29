b657ce94d7820b1d1406aa56d05dc428a4e9ff04
Frances Pasulohan <francespasulohan@gmail.com>
2024-10-29 12:50:28 +0800

    use AppShowHide component and updated styles

--- src/components/RestApi.vue
+++ src/components/RestApi.vue
@@ -2,8 +2,10 @@
 import { onMounted, ref } from 'vue'
 import { useStore } from '../stores/store'
 import { storeToRefs } from 'pinia'
+import AppShowHide from './AppShowHide.vue'
+import { NAV } from '../constants'
 
-const { name } = storeToRefs(useStore())
+const { name, btnClass, navState } = storeToRefs(useStore())
 
 // fetch 6 images from the API: https://dog.ceo/api/breeds/image/random
 const fetchImages = async () => {
@@ -40,26 +42,43 @@ const showSection = ref(false)
 onMounted(() => {
   refreshImages()
 })
+
+const handleShowHideClick = () => {
+  navState.value.restapi.hideSection =!navState.value.restapi.hideSection
+}
 </script>
 
 <template>
-  <div>
-    <h1>Hello {{ name }}</h1>
+  <AppShowHide
+    class="mt-5 py-1"
+    :show="navState.restapi.hideSection"
+    @toggle="handleShowHideClick"
+    :label="NAV.RESTAPI"
+  />
 
-    <!-- 2x3 grid layout -->
-    <section class="">
-      <h1 class="" v-if="!images.length">Loading</h1>
-      <Suspense>
-        <div class="grid grid-cols-2 gap-4" v-if="images.length">
-          <div v-for="img in images" :key="img">
-            <img :src="img" alt="dog" class="h-64 rounded-xl" />
-          </div>
+  <!-- 2x3 grid layout -->
+  <section class="" v-if="!navState.restapi.hideSection">
+    <h1 class="text-2xl font-bold">
+      Hello, {{ name }}
+      <span v-if="!images.length">Loading...</span>
+    </h1>
+    <Suspense>
+      <div class="grid grid-cols-2 gap-4 mt-3" v-if="images.length">
+        <div v-for="img in images" :key="img">
+          <img :src="img" alt="dog" class="h-64 rounded-xl mx-auto" />
         </div>
-        <template #fallback> Loading... </template>
-      </Suspense>
-      <button @click="refreshImages" class="p-3 m-3 rounded-xl bg-slate-500">Refresh</button>
-    </section>
-  </div>
+      </div>
+      <template #fallback> Loading... </template>
+    </Suspense>
+    <div class="flex flex-col my-6">
+      <button
+        :class="btnClass"
+        @click="refreshImages"
+      >
+        Refresh
+      </button>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped></style>
