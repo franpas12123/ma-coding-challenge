8b0d0008fb44cbb6d39db576f02bddc4204ad043
Frances Pasulohan <francespasulohan@gmail.com>
2024-10-29 13:06:41 +0800

    remove routes

--- src/router.js
+++ src/router.js
@@ -4,9 +4,6 @@ import HomeView from './views/HomeView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/i-love-arrays', component: HomeView },
-  { path: '/rest-api', component: HomeView },
-  // { path: '/', component: HomeView },
 ]
 
 const router = createRouter({
