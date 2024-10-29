41bd374e02465349591b287db577c35d30286802
Frances Pasulohan <francespasulohan@gmail.com>
2024-10-29 10:46:34 +0800

    move dark mode states and classes in store

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,8 +1,21 @@
 // create a basic pinia store
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+
 export const useStore = defineStore('store', () => {
   const name = ref('')
 
-  return { name }
+  // toogle light and dark mode
+  const isDarkMode = ref(false)
+  const toggleDarkMode = () => {
+    isDarkMode.value = !isDarkMode.value
+  }
+  const textareaClass = computed(() => {
+    return isDarkMode.value ? 'textarea-dark' : 'textarea-light'
+  })
+  const btnClass = computed(() => {
+    return isDarkMode.value ? 'btn-dark' : 'btn-light'
+  })
+
+  return { name, isDarkMode, toggleDarkMode, textareaClass, btnClass }
 })
