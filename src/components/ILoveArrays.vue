f74b1b57faa794126440f106291c135e4bf911e7
Frances Pasulohan <francespasulohan@gmail.com>
2024-10-29 14:07:40 +0800

    wrap with div to fix warning

--- src/components/ILoveArrays.vue
+++ src/components/ILoveArrays.vue
@@ -11,7 +11,10 @@ const result = ref('')
 const errorMsg = ref('')
 
 const handleClick = () => {
-  const inputs = input.value.split(',').map((num) => num.trim()).filter((num) => num !== '')
+  const inputs = input.value
+    .split(',')
+    .map((num) => num.trim())
+    .filter((num) => num !== '')
   if (inputs.length > 50) {
     errorMsg.value = 'You can only enter up to 50 numbers.'
     return
@@ -54,36 +57,45 @@ const handleShowHideClick = () => {
 }
 </script>
 <template>
-  <AppShowHide class="mt-5 py-1" :show="navState.iloveArrays.hideSection" @toggle="handleShowHideClick" :label="NAV.ILOVEARRAYS" />
-  <section v-if="!navState.iloveArrays.hideSection">
-    <h1 class="text-2xl font-bold">Hello, {{ name }}</h1>
-    <p>
-      The textbox will accept a comma delimited numbers which will be converted into an
-      array. The numbers will be any number from 0 to 9 and up to 50 elements – a number
-      may appear multiple times. When the user clicked the submit button, it will display
-      all the unique numbers with their corresponding counts in the results section. Sort
-      the result by count from highest to lowest then by number highest to lowest. Example
-      input: 4, 5, 6, 7, 8, 9, 8, 4, 5, 4 Example output: (Number - Count) 4 - 3 8 - 2 5 -
-      2 9 - 1 7 - 1 6 – 1
-    </p>
-    <div class="flex flex-col">
-      <p v-if="result.length" class="mt-2 font-bold">
-        (Number - Count) <span class="text-green-500">{{ result }}</span>
+  <div>
+    <AppShowHide
+      class="mt-5 py-1"
+      :show="navState.iloveArrays.hideSection"
+      @toggle="handleShowHideClick"
+      :label="NAV.ILOVEARRAYS"
+    />
+    <section v-if="!navState.iloveArrays.hideSection">
+      <h1 class="text-2xl font-bold">Hello, {{ name }}</h1>
+      <p>
+        The textbox will accept a comma delimited numbers which will be converted into an
+        array. The numbers will be any number from 0 to 9 and up to 50 elements – a number
+        may appear multiple times. When the user clicked the submit button, it will
+        display all the unique numbers with their corresponding counts in the results
+        section. Sort the result by count from highest to lowest then by number highest to
+        lowest. Example input: 4, 5, 6, 7, 8, 9, 8, 4, 5, 4 Example output: (Number -
+        Count) 4 - 3 8 - 2 5 - 2 9 - 1 7 - 1 6 – 1
       </p>
-      <p v-if="errorMsg.length" class="text-red-500 font-medium">{{ errorMsg }}</p>
-      <textarea
-        :class="isDarkLightClass"
-        class="mt-2"
-        name="textarea"
-        id="textarea"
-        v-model="input"
-        @change="errorMsg = ''"
-        @input="errorMsg = ''"
-      />
+      <div class="flex flex-col">
+        <p v-if="result.length" class="mt-2 font-bold">
+          (Number - Count) <span class="text-green-500">{{ result }}</span>
+        </p>
+        <p v-if="errorMsg.length" class="text-red-500 font-medium">{{ errorMsg }}</p>
+        <textarea
+          :class="isDarkLightClass"
+          class="mt-2"
+          name="textarea"
+          id="textarea"
+          v-model="input"
+          @change="errorMsg = ''"
+          @input="errorMsg = ''"
+        />
 
-      <button :class="isDarkLightClass" class="mt-3" @click="handleClick">Submit</button>
-    </div>
-  </section>
+        <button :class="isDarkLightClass" class="mt-3" @click="handleClick">
+          Submit
+        </button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
